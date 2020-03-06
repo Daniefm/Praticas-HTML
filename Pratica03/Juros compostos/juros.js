3f9c398f0ee441bc8c956ff71aee28f4a725b0ce
Daniel Fernandes <56447026+Daniefm@users.noreply.github.com>
2020-03-06 12:53:13 -0300

    Add files via upload

---
+++ Pratica03/Juros compostos/juros.js
@@ -0,0 +1,12 @@
+//Captura os valores 
+var captini = parseInt (prompt ("Informe a capital inicial: "));
+var taxa = parseInt (prompt ("Informe a taxa de juros: "));
+var prazo = parseInt (prompt ("Informe o tempo de investimento em meses: "));
+
+//transformando a taxa em %
+taxa = taxa/100;
+
+var montante = captini * Math.pow((1 + taxa), prazo);
+
+document.write("O montante é: ", montante, "<br><br>");
+document.write ("Os juros totais são de: ", montante - captini);
---
+++ Pratica03/Volume esfera/volume.js
@@ -0,0 +1,5 @@
+var raio = prompt ("Digite o raio da esfera: ");
+
+volume = 4 / 3 * Math.PI * Math.pow(raio, 3);
+
+document.write("O volume da esfera é = ", volume);
